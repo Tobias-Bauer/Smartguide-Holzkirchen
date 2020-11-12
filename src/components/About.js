c68218cdac86320d6bd153a0c6a1c834ef205d6e
Tobias-Bauer <tobias.bauer@mbtj.de>
2020-11-12 19:22:45 +0100

    About-Page design update

--- src/components/About.js
+++ src/components/About.js
@@ -7,51 +7,72 @@ class About extends React.Component{
     constructor(props){
         super(props);
         if(this.props.match.params.language === "en"){
-            this.state = {style1: {}, style2: green, language: "en"}
+            this.state = {style1: {}, style2: green, language: "en", mehr: true}
         }else{
-            this.state = {style1: green, style2: {}, language: "de"}
+            this.state = {style1: green, style2: {}, language: "de", mehr: true}
         }
     }
     render() {
         return(
-        <div className="aboutContent">
+        <div>
             <div className="header">
-                <Link to={"/home/"+(this.state.style1 === green? "de":"en")}>
-                    <h1>Smartguide</h1>
-                    <h2>Holzkirchen</h2>
-                </Link>
-                <div className="languageButton">
-                    {localStorage.getItem('darkmode')==="true"?<p onClick={() => this.setState({style1: green, style2: {}, language: "de"})} style={this.state.style1 === green?{backgroundColor: "#A0C510",color: "black"}:{}}>DE</p>:
-                    <p onClick={() => this.setState({style1: green, style2: {}, language: "de"})} style={this.state.style1}>DE</p>}
-                    {localStorage.getItem('darkmode')==="true"?<p onClick={() => this.setState({style1: {}, style2: green, language: "en"})} style={this.state.style2 === green?{backgroundColor: "#A0C510",color: "black"}:{}}>EN</p>:
-                    <p onClick={() => this.setState({style1: {}, style2: green, language: "en"})} style={this.state.style2}>EN</p>}
+            <Link to={"/home/"+(this.state.style1 === green? "de":"en")}>
+                <h1>Smartguide</h1>
+                <h2>Holzkirchen</h2>
+            </Link>
+            <div className="languageButton">
+                {localStorage.getItem('darkmode')==="true"?<p onClick={() => this.setState({style1: green, style2: {}, language: "de"})} style={this.state.style1 === green?{backgroundColor: "#A0C510",color: "black"}:{}}>DE</p>:
+                <p onClick={() => this.setState({style1: green, style2: {}, language: "de"})} style={this.state.style1}>DE</p>}
+                {localStorage.getItem('darkmode')==="true"?<p onClick={() => this.setState({style1: {}, style2: green, language: "en"})} style={this.state.style2 === green?{backgroundColor: "#A0C510",color: "black"}:{}}>EN</p>:
+                <p onClick={() => this.setState({style1: {}, style2: green, language: "en"})} style={this.state.style2}>EN</p>}
+            </div>
+        </div>
+        <div className="aboutContent">
+            <div className="shiftRight">
+                    <h3>Grüß Gott</h3>
+                <div className="imgContainer">
+                    <div></div>
+                    <img alt="Team" src={require("../images/group_photo.JPG")}/>
                 </div>
             </div>
+            <h4>Grüß Gott</h4>
             {this.state.language==="de"?
-            <div>
-                <p>Grüß Gott <br/>
+            <div className="aboutText">
+                <p>
                 Wir, das Projektseminar „Smartguide für Holzkirchen“ des Staatlichen Gymnasiums Holzkirchen, heißen Sie herzlich Willkommen auf unserer Website (in der App). Über einige Monate hinweg haben wir uns intensiv mit unserem Heimatort Holzkirchen beschäftigt und einen Smartguide über die bedeutsamsten Standorte erstellt. Zu diesen wird Ihnen jeweils ein Audioguide und ein Informationstext mit authentischen Bildern zur Verfügung gestellt, damit Sie sich einen realistischen Eindruck verschaffen können. Dabei wünschen wir Ihnen viel Spaß.
                 <br/><br/>
                 Ihr Projektseminar 
                 </p>
-                <img alt="Team" src={require("../images/TeufelsgrabenSatelit.jpeg")}/>
-                <h1>Mitglieder Beispiel:</h1>
-                <div className="grid">
-                    <p>Leonard von Foerster:</p>
-                    <ul>
-                        <li>Erstellung der App mit Flutter</li>
-                        <li>Station zum Alten Rathaus</li>
-                    </ul>
-                    <p>Fabian Kuske:</p>
-                    <ul>
-                        <li>Übertragung der Inhalte von Website zur App</li>
-                        <li>Station zum Bahnhof</li>
-                    </ul>
-                    <p>Tobias Bauer:</p>
-                    <ul>
-                        <li>Erstellung der Website mit ReactJS</li>
-                        <li>Station zum Teufelsgraben</li>
-                    </ul>
+                <div className="aboutTeam">
+                    {this.state.mehr?
+                    <h1 onClick={() => this.setState({mehr: !this.state.mehr})}>
+                        <svg width="21" height="10" viewBox="0 0 21 3" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <line x1="1.31134e-07" y1="1.5" x2="21" y2="1.5" stroke="#A0C510" stroke-width="3"/>
+                        </svg>
+                        weniger zum Team</h1>:
+                        <h1 onClick={() => this.setState({mehr: !this.state.mehr})}>
+                            <svg width="21" height="15" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <line x1="10.5" y1="-6.55671e-08" x2="10.5" y2="21" stroke="#A0C510" stroke-width="3"/>
+                                <line x1="1.31134e-07" y1="10.5" x2="21" y2="10.5" stroke="#A0C510" stroke-width="3"/>
+                            </svg>
+                            mehr zum Team</h1>}
+                    {this.state.mehr?<div className="grid">
+                        <p>Leonard von Foerster:</p>
+                        <ul>
+                            <li>Erstellung der App mit Flutter</li>
+                            <li>Station zum Alten Rathaus</li>
+                        </ul>
+                        <p>Fabian Kuske:</p>
+                        <ul>
+                            <li>Übertragung der Inhalte von Website zur App</li>
+                            <li>Station zum Bahnhof</li>
+                        </ul>
+                        <p>Tobias Bauer:</p>
+                        <ul>
+                            <li>Erstellung der Website mit ReactJS</li>
+                            <li>Station zum Teufelsgraben</li>
+                        </ul>
+                    </div>:null}
                 </div>
             </div>:
             <div>
@@ -60,8 +81,15 @@ class About extends React.Component{
                 <br/><br/>
                 Your Smartguide-Team
                 </p>
-                <img alt="team" src={require("../images/TeufelsgrabenSatelit.jpeg")}/>
             </div>}
+            <Link className="bottom" to={"/map/"+(this.state.style1 === green? "de":"en")}>
+                <h5>zum Guide 
+                    <svg width="10" height="16" viewBox="0 0 10 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M1 1L8 8L1 15" stroke="white" stroke-width="2"/>
+                    </svg>
+                </h5>
+            </Link>
+        </div>
         </div>
         )
     }
